7fdcf95e5f828978c8fb0d771ed361cac8f761fd
manuel-salvador <manu.sacr@hotmail.com>
2024-11-09 12:25:51 -0300

    fix web3auth

--- frontend/src/lib/web3auth.ts
+++ frontend/src/lib/web3auth.ts
@@ -1,20 +1,20 @@
-import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from "@web3auth/base";
-import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from '@web3auth/base';
+import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider';
+import { Web3Auth } from '@web3auth/modal';
 
-const clientId = "BB48JMOP4FhuYGCuJ39TrSOcHRX6YvG9Uf_bKv0lLQupXZJyzA6dX1tb-Sgq5l3ql_4P1RUZzPwfnBMdpDdBslM";
+const clientId = process.env.NEXT_PUBLIC_WEB3_CLIENT_ID ?? '';
 
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
-  chainId: "0xaa36a7",
-  rpcTarget: "https://rpc.ankr.com/eth_sepolia",
+  chainId: '0xaa36a7',
+  rpcTarget: 'https://rpc.ankr.com/eth_sepolia',
   // Avoid using public rpcTarget in production.
   // Use services like Infura, Quicknode etc
-  displayName: "Ethereum Sepolia Testnet",
-  blockExplorerUrl: "https://sepolia.etherscan.io",
-  ticker: "ETH",
-  tickerName: "Ethereum",
-  logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+  displayName: 'Ethereum Sepolia Testnet',
+  blockExplorerUrl: 'https://sepolia.etherscan.io',
+  ticker: 'ETH',
+  tickerName: 'Ethereum',
+  logo: 'https://cryptologos.cc/logos/ethereum-eth-logo.png',
 };
 
 const privateKeyProvider = new EthereumPrivateKeyProvider({
